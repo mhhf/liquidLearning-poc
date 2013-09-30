c26a83879827028254f671241f8e30d821f71ec1
mhhf <mhhf@freenet.de>
2013-09-30 16:37:25 +0200

    init project

---
+++ client/router.js
@@ -0,0 +1,20 @@
+Router.configure({
+
+  layout: 'defaultLayout',
+
+  notFoundTemplate: 'notFound',
+
+  loadingTemplate: 'loading',
+
+  renderTemplates: { 
+    'footer': { to: 'footer' },
+  }
+
+});
+
+
+Router.map(function() { 
+  this.route('home', {
+			path: '/'
+	});
+});
---
+++ client/views/main.js
@@ -0,0 +1,54 @@
+Session.set('typing',true);
+
+Template.home.helpers({
+	typing: function(){
+		return Session.get('typing');	
+	}
+});
+
+Template.home.rendered = function(){
+	 $("select[name='insertText']").selectpicker({
+			 style: 'btn-primary', 
+			 menuStyle: 'dropdown-inverse'
+	 });
+	 // TODO #editor: break sentences on new lines.
+	 // TODO #editor: allow to insert sync informations.
+	 // TODO #editor: parse sentances
+	 // TODO #editor: add button groub for text manipulation > add sync button
+	 // XXX #editor: change editor to CodeMirror?
+	 $('#editor').wysiwyg();
+};
+
+Template.home.events({
+	"click button[name=play]" : function(){
+		Session.set('typing',false);
+		var s = $('#editor')[0].innerText.replace(/\r\n|\n\r|\r|\n/g, '').split('.');
+		s = _.filter(s, function(str){
+			return !!str && str != "";
+		});
+		s = _.map(s, function(str){
+			return str+'.';
+		});
+		console.log(s);
+		
+	},
+	"click button[name=stop]" : function(){
+		Session.set('typing',true);
+	},
+	"click .select" : function(e,t){
+		// TODO #textSelect: Load different texts from a database
+		var val;
+		switch(e.target.textContent) {
+			case "none":
+				val = '';
+				break;
+			case "LSD":
+				val = 'LSD is the best known and most researched psychedelic.<br /> It is the standard against which all other psychedelics are compared.<br /> It is active at extremely low doses and is most commonly available on blotter or in liquid form.';
+				break;
+			default:
+				val = '';
+		}
+
+	 $('#editor').html(val);
+	}
+});
