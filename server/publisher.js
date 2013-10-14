b9df0816b11468966b8c3e28ede0011a036171e2
mhhf <mhhf@freenet.de>
2013-10-14 18:49:30 +0200

    [+] subscription based client - server interaction

--- server/publisher.js
+++ server/publisher.js
@@ -1,6 +1,45 @@
 Meteor.publish('text', function(text){
 	
-	console.log(text);
+	// TODO #performance: why is this method called twice?
+	genObjectSync = Meteor._wrapAsync(genObjectAsync);
 
-	return Syncs.find();
-})
+	var endResult = [],
+	result = _.sortBy(genObjectSync( text ), function(o){
+		return o.i;
+	});
+	result = _.pluck(result, 'id' );
+
+	return Syncs.find({_id: {$in: result}});
+});
+
+
+var genObjectAsync = function( text, cb ){
+		var processed = [];
+		var queue = 0;
+
+		var hash, tts, mp3Link;
+		for(var i=0; i<text.length; i++) {
+			hash = MD5.hash( text[i] ).toString();
+			tts = Syncs.findOne({ hash: hash });
+			if(!tts){
+				queue++;
+
+				TtsEngine.synthesize({
+					text : text[i],
+					lang : "en"   
+				},function( obj ){ 
+					var id = Syncs.insert( obj );
+					processed.push( { id:id, i:i} );
+					if( --queue == 0 ) {
+						cb(null, processed);
+					}
+				});
+
+			} else {
+				processed.push( {id:tts._id, i:i} );
+			}
+		}
+		if( queue == 0 ){
+			cb(null, processed);
+		}
+}
--- server/uploadHandler.js
+++ server/uploadHandler.js
@@ -17,58 +17,6 @@ Meteor.methods({
 
 	clear: function(){
 		Syncs.remove({});
-	},
-
-	// return the interpreted tts format to the client
-	//
-	// @o:  sentences []
-	//
-	// @return: processed [{ hash, text, mp3Link }]
-	process : function( text ){
-
-		genObjectSync = Meteor._wrapAsync(genObjectAsync);
-
-		var endResult = [],
-		result = _.sortBy(genObjectSync( text ), function(o){
-			return o.i;
-		});
-
-		for(var i = 0; i< result.length; i++ ){
-			endResult.push(Syncs.findOne({ _id: result[i].id }));
-		}
-		return endResult;
 	}
 
 });
-
-
-var genObjectAsync = function( text, cb ){
-		var processed = [];
-		var queue = 0;
-
-		var hash, tts, mp3Link;
-		for(var i=0; i<text.length; i++) {
-			hash = MD5.hash( text[i] ).toString();
-			tts = Syncs.findOne({ hash: hash });
-			if(!tts){
-				queue++;
-
-				TtsEngine.synthesize({
-					text : text[i],
-					lang : "en"   
-				},function( obj ){ 
-					var id = Syncs.insert( obj );
-					processed.push( { id:id, i:i} );
-					if( --queue == 0 ) {
-						cb(null, processed);
-					}
-				});
-
-			} else {
-				processed.push( {id:tts._id, i:i} );
-			}
-		}
-		if( queue == 0 ){
-			cb(null, processed);
-		}
-}
