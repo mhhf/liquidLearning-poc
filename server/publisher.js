52da7cee983710d9d4ba5870e7925e13eefee66b
mhhf <mhhf@freenet.de>
2013-11-29 13:38:01 +0100

    [+] TTS interface cleanup with Fibers

--- server/publisher.js
+++ server/publisher.js
@@ -1,3 +1,5 @@
+var Fiber = Npm.require('fibers');
+
 /*
  * Convert a an array of sentances to a syncObject Array 
  * publish the collection to the client
@@ -5,7 +7,6 @@
 Meteor.publish('text', function( text ){
 	
 	// TODO #performance: why is this method called twice?
-	genObjectSync = Meteor._wrapAsync(genObjectAsync);
 
 	var endResult = [],
 	result = _.sortBy(genObjectSync( text ), function(o){
@@ -16,39 +17,52 @@ Meteor.publish('text', function( text ){
 	return Syncs.find({_id: {$in: result}});
 });
 
-
 // converts an text array to a syncObject id's:
 //
 // 1. synthesize the text to an audio file, if neccecery
 // 2. provides an array of syncObject id's from the database
 //
 var genObjectAsync = function( text, cb ){
-		var processed = [];
-		var queue = 0;
+  var processed = [];
+  var queue = 0;
 
-		var hash, tts, mp3Link;
-		for(var i=0; i<text.length; i++) {
-			hash = MD5.hash( text[i] ).toString();
-			tts = Syncs.findOne({ hash: hash });
-			if(!tts){
-				queue++;
+  var t0 = +(new Date());
 
-				TtsEngine.synthesize({
-					text : text[i],
-					lang : "en"   
-				},function( obj ){ 
-					var id = Syncs.insert( obj );
-					processed.push( { id:id, i:i} );
-					if( --queue == 0 ) {
-						cb(null, processed);
-					}
-				});
+  var hash, tts, mp3Link;
+  for(var i=0; i<text.length; i++) {
+    hash = MD5.hash( text[i] ).toString();
+    tts = Syncs.findOne({ hash: hash });
+    if(!tts){
+      queue++;
+      
+
+        Fiber( function(){
+
+          // get the synth object
+          var obj = TtsEngine.synthesize({
+            text : text[i],
+            lang : "en"
+          });
+
+          var id = Syncs.insert( obj );
+          processed.push( { id:id, i:i} );
+          if( --queue == 0 ) {
+            cb && cb(null, processed);
+            cb = null;
+          }
+
+        }).run();
 
 			} else {
 				processed.push( {id:tts._id, i:i} );
 			}
 		}
 		if( queue == 0 ){
-			cb(null, processed);
+			cb && cb(null, processed);
 		}
 }
+var genObjectSync = Meteor._wrapAsync(genObjectAsync);
+
+
+
+
--- server/uploadHandler.js
+++ server/uploadHandler.js
@@ -3,7 +3,8 @@ Meteor.startup( function(){
 	// Initialize the Text to speach engine
 	// TODO #tts #filepicker: rename key to filepickerKey
 	TtsEngine = new TTS({
-		key: 'A64VCBSSpKplhCYDX8Uvwz',
+    awsId: 'AKIAIAZ7MFEB2LGSKTYQ',
+    awsSecret: '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',
 		s3bucket: 'liquidlearning-poc',
 		service: "tts"
 	});
