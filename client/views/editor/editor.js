5fe4c56cfc989175bd8eec0cf7dcb9159ac8824d
mhhf <mhhf@freenet.de>
2013-12-01 18:37:41 +0100

    [r] instantPreview from app to package

--- client/views/editor/editor.js
+++ client/views/editor/editor.js
@@ -1,104 +1,22 @@
-//
-//
-// TODO: write an image preloader which loads the images in the background
-//
-// TODO #editor: visualize:
-//  * which slide is currently shown
-//  * where are markdown errors
-//  * where the slide begins and where it ends
-//  * highlight the notes and the slide code
-//
-// holds the AST of the parsed Slides
-var slides = null;
-// pointer to the template scope
-var _template;
-
-var currentLine = -1;
-var currentSlide = -1;
-
 Template.editor.rendered = function(){
-	// TODO #editor: allow to insert sync informations.
-  // TODO #preview: always resize the preview with the ratio of 4/3
-  
-  _template = this;
-	
-	editor = CodeMirror(this.find('#editor'),{
-		value: "",
-		mode:  "markdown",
-		lineNumbers: true,
-		extraKeys: {"Ctrl-J": "autocomplete"}
-	});
-
-  // When the markup is changed
-	editor.on('change', function(i,o){
-    // Parse the markup
-    parseSlides();
-    // update the display
-    // pick what to display
-    positionHasChanged();
-    // display it
-    displaySlide();
-	});
-	
-  editor.on('cursorActivity', function(i,o){
-    if( positionHasChanged() ) { // and slide changed
-      displaySlide(); // display the changed slide
-    }
-  });
 
 };
 
-// render the current slide to the preview
-displaySlide = function( slide ){
-  var html = slides[ slide == null?currentSlide:slide ].md;
-  _template.find('#preview').innerHTML= marked( html );
-}
-
-
-// returns the current slide, which should be displayed
-// this is triggered by the current cursor position in the editor
-getSlide = function(){
-  for (var i=0; i < slides.length; i++) {
-    if( slides[i].from <= currentLine 
-        && slides[i].to >= currentLine )
-        return i;
-  }
-  return -1;
-}
-
-// Parse the slides to a AST
-parseSlides = function(){
-  slides = SlideParser.parse( editor.getValue() );
-}
-
-// updates the current line and current slide positions, 
-// depending on the change of the current cursor position
-// returns if the current slide has changed
-positionHasChanged = function(){
-  if ( currentLine != editor.getCursor().line+1 ) {// O(1)
-    var _currentSlide = currentSlide;
-    return _currentSlide != (currentSlide = getSlide( currentLine = editor.getCursor().line+1 )); // O(n)
-  }
-  return false;
-}
 
 Template.editor.events({
 	"click button[name=play]" : function(){
 
-		// Preprocess the text:
-		// make an array of sentences
-    //
-    //
-    // parse the slides again
-    parseSlides(); // remove?
+    var slides = InstantPreview.getSlides();
 
-    var value = [];
+    // collect all notes
+    var notes = [];
     _.each(slides, function(slide){
-      value = value.concat( slide.notes );
+      notes = notes.concat( slide.notes );
     });
 
-		Session.set('text',value);
+		Session.set('text',notes);
     Session.set('slides', slides);
+
 		Router.go('player');
 	},
 	"change select" : function(e,t){
@@ -121,6 +39,6 @@ Template.editor.events({
 				val = '';
 		}
 
-		editor.setValue(val);
+    InstantPreview.setMarkdown( val );
 	}
 });
