afa0f18654cee80601a8d3f585d5f6063bcd43b7
mhhf <mhhf@freenet.de>
2014-05-21 19:29:23 +0200

    add todo

--- collections/schemas/schemas.js
+++ collections/schemas/schemas.js
@@ -1,14 +1,15 @@
+// [TODO] - fucking ged rid of units -> units just files in the coruse structure
 Schemas.Units = [
   {state: {
     type: String,
     defaultValue: 'ready'
   }},
-  ACLInterface.schema,
-  Schemas.Common.Owner,
   Schemas.Common.Git,
-  Schemas.Common.Stars,
   Schemas.Common.LLMD,
   Schemas.Common.Activity,
+  ACLInterface.schema,
+  Schemas.Common.Owner,
+  Schemas.Common.Stars,
   Schemas.Common.Common
 ];
 
